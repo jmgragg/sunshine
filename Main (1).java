84e299fc2713bd6117b22db6ae353f315def18a0
jmgragg <165817939+jmgragg@users.noreply.github.com>
2024-04-16 08:50:04 -0700

    Update Main (1).java

---
+++ Main (1).java
@@ -0,0 +1,13 @@
+class Main{
+  public static void water() {
+    System.out.println("Water");
+  }
+  public static void shine() {
+    System.out.println("Shine");
+  }
+}
+public static void main(String[] args) {
+  water();
+  shine();
+}
+}
